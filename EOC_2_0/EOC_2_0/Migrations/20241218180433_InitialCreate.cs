207bda467d6ff0eae61a5f5e71e7ff2f2cac7770
Raaazzy <dariyaomirbekova52@gmail.com>
2024-12-18 21:19:19 +0300

    added migrations

---
+++ EOC_2_0/EOC_2_0/Migrations/20241218180433_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EOC_2_0.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFilePath = Path.Combine("Migrations", "SQLScripts", "init.sql");
+            if (File.Exists(sqlFilePath))
+            {
+                var sqlScript = File.ReadAllText(sqlFilePath);
+                migrationBuilder.Sql(sqlScript);
+            }
+            else
+            {
+                throw new FileNotFoundException($"SQL файл {sqlFilePath} не найден.");
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClassifiedVerbs");
+
+            migrationBuilder.DropTable(
+                name: "NewVerbs");
+
+            migrationBuilder.DropTable(
+                name: "Nouns");
+        }
+    }
+}
